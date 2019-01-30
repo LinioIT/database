f2a39133fd1457393d9c0bb5cf0f0c8ac08ccb4b
Matheus Martins <mathrm@gmail.com>
2019-01-30 16:39:53 -0200

    add unit tests to work with or without double quotes in the param names.

--- tests/Query/Transformer/NamedArrayParameterTest.php
+++ tests/Query/Transformer/NamedArrayParameterTest.php
@@ -8,7 +8,32 @@ use PHPUnit\Framework\TestCase;
 
 class NamedArrayParameterTest extends TestCase
 {
-    public function testItDoesTransformQueryWithOneArrayParameter(): void
+    public function testItDoesTransformQueryWithOneArrayParameterUsingDoubleQuotes(): void
+    {
+        $query = 'SELECT id, email FROM users WHERE uuid in (:uuid)';
+
+        $params = [
+            ':uuid' => [
+                'abc-13',
+                'def-456'
+            ],
+        ];
+
+        $expectedQuery = 'SELECT id, email FROM users WHERE uuid in (:uuid_0, :uuid_1)';
+
+        $expectedParams = [
+            ':uuid_0' => 'abc-13',
+            ':uuid_1' => 'def-456',
+        ];
+
+        $transformer = new NamedArrayParameter();
+        $transformer->execute($query, $params);
+
+        $this->assertEquals($expectedQuery, $query);
+        $this->assertEquals($expectedParams, $params);
+    }
+
+    public function testItDoesTransformQueryWithOneArrayParameterNotUsingDoubleQuotes(): void
     {
         $query = 'SELECT id, email FROM users WHERE uuid in (:uuid)';
 
@@ -33,7 +58,40 @@ class NamedArrayParameterTest extends TestCase
         $this->assertEquals($expectedParams, $params);
     }
 
-    public function testItDoesTransformQueryWithTwoArrayParameters(): void
+    public function testItDoesTransformQueryWithTwoArrayParametersUsingDoubleQuotes(): void
+    {
+        $query = 'SELECT id, email FROM users WHERE uuid in (:uuid) and status IN (:status)';
+
+        $params = [
+            ':uuid' => [
+                'abc-13',
+                'def-456'
+            ],
+            ':status' => [
+                1,
+                5,
+                6
+            ],
+        ];
+
+        $expectedQuery = 'SELECT id, email FROM users WHERE uuid in (:uuid_0, :uuid_1) and status IN (:status_0, :status_1, :status_2)';
+
+        $expectedParams = [
+            ':uuid_0' => 'abc-13',
+            ':uuid_1' => 'def-456',
+            ':status_0' => 1,
+            ':status_1' => 5,
+            ':status_2' => 6,
+        ];
+
+        $transformer = new NamedArrayParameter();
+        $transformer->execute($query, $params);
+
+        $this->assertEquals($expectedQuery, $query);
+        $this->assertEquals($expectedParams, $params);
+    }
+
+    public function testItDoesTransformQueryWithTwoArrayParametersNotUsingDoubleQuotes(): void
     {
         $query = 'SELECT id, email FROM users WHERE uuid in (:uuid) and status IN (:status)';
 
