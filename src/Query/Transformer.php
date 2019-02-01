834a5f02b441ad242866ce9f0edf4518fc8cfef0
Matheus Martins <mathrm@gmail.com>
2019-02-01 16:32:29 -0200

    add exception doc block to interface method declaration.

--- src/Query/Transformer.php
+++ src/Query/Transformer.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Linio\Component\Database\Query;
 
+use Linio\Component\Database\Exception\InvalidQueryException;
+
 interface Transformer
 {
+    /**
+     * @throws InvalidQueryException
+     */
     public function execute(string &$query, array &$params = []): void;
 }
