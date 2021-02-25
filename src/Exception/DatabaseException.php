ac3d81f190247f5fad48ee8e50773fb862754e59
Maikel Salazar <maikelsalazar@gmail.com>
2021-02-25 12:06:23 -0600

    fix: cast datatypes for DatabaseException

--- src/Exception/DatabaseException.php
+++ src/Exception/DatabaseException.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Linio\Component\Database\Exception;
 
 use RuntimeException;
+use Throwable;
 
 class DatabaseException extends RuntimeException
 {
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    {
+        parent::__construct((string) $message, (int) $code, $previous);
+    }
 }
